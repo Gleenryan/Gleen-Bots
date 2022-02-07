e8c11dda61de2616dcc6b91c8d5d8e4df8c99a5a
Gleenryan <99118090+Gleenryan@users.noreply.github.com>
2022-02-07 08:33:04 +0700

    add_status
    adding status to the bot!

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const config = require("./config.json");
 const { Client, Intents } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
+const Status_bot = ["Music Bot", "Music Bot", "-info for some info"];
+
+
 client.on("ready", () => {
-    console.log(`${client.user.tag} ready to serve!`)
+    console.log(`${client.user.tag} is Online`)
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (Status_bot.length - 1) + 1);
+        client.user.setActivity(Status_bot[index], { type: "WATCHING" });
+    }, 10000);
 });
+
+
 
 client.login(config.token);
